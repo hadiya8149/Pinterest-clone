2411bbf3b4ee552f82f1a159b4d06c2169947711
hadiya8149 <hadiya8149@gmail.com>
2020-04-30 12:15:38 +0500

    Added database connection

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+#from memory_profiler import profile
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__,static_url_path='', static_folder='static', template_folder='templates')
+from sqlalchemy import Column, Integer, Text
+
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pin.db'
+db = SQLAlchemy(app)
+
+
+#@profile
+class PinClone(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, unique=False)
+    image = Column(Text, unique=False)
+
+
+db.create_all()
 
 
 @app.route('/')
